f45f286cdd7f97cde956c058a11ffb16e4529ffa
Anthicius <andrius.makseckas@gmail.com>
2026-01-03 10:11:48 +0200

    second commit

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,10 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import Button from "./Button";
 
 const Counter = () => {
-    const [count, setCount] = useState(0)
-  return (
-    <div>Counter</div>
-  )
-}
+  const [count, setCount] = useState(0);
+  const [color,setColor] = useState(true)
 
-export default Counter
+  const handleChange = (label) => {
+    console.log(label)
+    if (label === "Increment") {
+      setCount((prevCount) => prevCount + 1);
+    } else setCount((prevCount) => prevCount - 1);
+    if(count<0){
+      setColor(false)
+    } else setColor(true)
+  };
+
+  return (
+    <>
+      <Button onChange={handleChange} label="Increment" />
+      <Button onChange={handleChange} label="Decrement" />
+      <p style={{color: color?"black":"red "}}>{count}</p>
+    </>
+  );
+};
+
+export default Counter;

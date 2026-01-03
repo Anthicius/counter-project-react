37eb75c617cfddd15b494762f680ab2af6e2a331
Anthicius <andrius.makseckas@gmail.com>
2026-01-03 10:21:31 +0200

    final commit

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -3,23 +3,24 @@ import Button from "./Button";
 
 const Counter = () => {
   const [count, setCount] = useState(0);
-  const [color,setColor] = useState(true)
+
+
+  const textColor = count < 0 ? "red" : "black";
 
   const handleChange = (label) => {
-    console.log(label)
     if (label === "Increment") {
       setCount((prevCount) => prevCount + 1);
-    } else setCount((prevCount) => prevCount - 1);
-    if(count<0){
-      setColor(false)
-    } else setColor(true)
+    } else {
+      setCount((prevCount) => prevCount - 1);
+    }
   };
 
   return (
     <>
       <Button onChange={handleChange} label="Increment" />
       <Button onChange={handleChange} label="Decrement" />
-      <p style={{color: color?"black":"red "}}>{count}</p>
+      
+      <p style={{ color: textColor }}>{count}</p>
     </>
   );
 };
